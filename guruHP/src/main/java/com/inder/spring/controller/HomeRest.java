b17f1f9f21716ad86c373c93b0f77e3a7fe38077
inderinnovative <inderinnovatie@gmail.com>
2018-06-14 01:54:14 +0530

    rest test and gson

--- guruHP/src/main/java/com/inder/spring/controller/HomeRest.java
+++ guruHP/src/main/java/com/inder/spring/controller/HomeRest.java
@@ -3,32 +3,35 @@ package com.inder.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
 import com.inder.spring.model.Medicine;
 import com.inder.spring.service.HomeService;
 
-@RestController
+@Controller
 @RequestMapping("/rest/")
 public class HomeRest {
 	
 	@Autowired
 	HomeService homeService;
 
-	@RequestMapping(value="get/{player}/All.do", method={RequestMethod.GET,RequestMethod.POST})
-	public @ResponseBody Medicine getMedicine(@PathVariable String player){
+	@RequestMapping(value="get/{player}/All.do",produces={"application/json"} ,method={RequestMethod.GET,RequestMethod.POST})
+	public @ResponseBody String getMedicine(@PathVariable String player){
 		System.out.println("REST controller call"+player);
 		
-		Medicine m=new Medicine();
-		m.setId(12);
-		m.setName("Pented");
-		m.setUses(player);
-		m.setStock("5");
-		return m;
+		Medicine medicine=new Medicine();
+		medicine.setId(12);
+		medicine.setName("Pent:,ted");
+		medicine.setUses(player);
+		medicine.setStock("5");
+   Gson g=new  Gson();
+  return g.toJson(homeService.getAllRecord());
 		//return homeService.getAllRecord();
 	}
 }
